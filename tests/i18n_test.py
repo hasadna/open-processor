365b0c85df8f40ada0e7e6a4f47b8f152cf0b648
RoySegall <roy@segall.io>
2018-12-31 20:42:19 +0200

    Adding tests

--- tests/i18n_test.py
+++ tests/i18n_test.py
@@ -1,10 +1,31 @@
 """Testing the integration of our code."""
 
+from i18n import i18n
 
-def test_answer():
+
+def test_fields():
     """
-    Writing tests.
+    Testing the fields loading.
 
     Foo.
     """
-    assert 5 == 5
+    translator = i18n()
+
+    translator.set_fields({"food": "pizza"})
+
+    assert translator.translate_field("food") == "pizza"
+    assert translator.translate_field("pizza") is None
+
+
+def test_sheets():
+    """
+    Testing the fields loading.
+
+    Foo.
+    """
+    translator = i18n()
+
+    translator.set_sheets({"food": "pizza"})
+
+    assert translator.translate_sheet("food") == "pizza"
+    assert translator.translate_sheet("pizza") is None
